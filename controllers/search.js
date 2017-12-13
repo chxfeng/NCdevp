bd8cf3c38398117ff0a0fa40f14eb7c8352d2de8
xiaofeng.cheng <cxf_windy@126.com>
2017-12-13 17:07:01 +0800

    城市test1

--- controllers/search.js
+++ controllers/search.js
@@ -4,14 +4,14 @@ exports.index = function (req, res, next) {
   var q = req.query.q;
   q = encodeURIComponent(q);
   //res.redirect('https://www.google.com.hk/#hl=zh-CN&q=site:cnodejs.org+' + q);
-
+    console.info('城市test');
   //https://map.baidu.com/?qt=sub_area_list&from=mapapi&areacode=1&level=1&from=mapapi
-    http.get('https://map.baidu.com/?qt=sub_area_list&from=mapapi&areacode=1&level=1&from=mapapi',function(req,res){
+    http.get('https://map.baidu.com/?qt=sub_area_list&from=mapapi&areacode=1&level=1&from=mapapi',function(req1,res1){
         var html='';
-        req.on('data',function(data){
+        req1.on('data',function(data){
             html+=data;
         });
-        req.on('end',function(){
+        req1.on('end',function(){
             console.info(html);
         });
     });
